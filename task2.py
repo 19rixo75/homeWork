6340c3f2b828f62ef36df3821559aef57588e288
19rixo75 <19rixo75@gmail.com>
2021-03-24 20:04:53 +0300

    Home Work 2_V2

--- task2.py
+++ task2.py
@@ -7,3 +7,18 @@ for x in range(0, 100):
         print(x, ' Buzz')
     else:                   # Можно убрать, если не нужен весь список значений
         print(x)            #
+
+
+# Variant 2 (Вопрос: Почему функция выводит последнее значение None?)
+
+# n = int(input('Введите верхнее значеие диапазона от 0 до '))
+# def add(n):
+#     for x in range(0, n):
+#         if x % 3 == 0 and x % 5 == 0:
+#             print(x, ' FizzBuzz')
+#         elif x % 3 == 0:
+#             print(x, ' Fizz')
+#         elif x % 5 == 0:
+#             print(x, ' Buzz')
+#
+# print(add(n))
