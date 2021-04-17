c7ef03da1bce7e9c811d56bbb6d8527704917bb3
19rixo75 <19rixo75@gmail.com>
2021-04-17 23:53:48 +0300

    OS_HW

--- os_home_work.py
+++ os_home_work.py
@@ -1,4 +1,5 @@
 import os
+import time
 
 file_list = os.listdir('needs_sorting')
 os.makedirs('text_files', exist_ok=True)
@@ -10,11 +11,11 @@ for x in file_list:
         to_file = os.path.join('text_files', x)
         os.rename(from_file, to_file)
         with open('text_files/create_folder_text.txt', 'a', encoding='utf8') as write_file:
-            write_file.write('\nName file: ' + x + '\nDate created file: ' + str(os.stat(to_file).st_ctime) + '\nSize file: ' + str(os.stat(to_file).st_size) + '\n')
+            write_file.write('\nName file: ' + x + '\nDate created file: %s' % time.ctime(os.path.getctime(to_file)) + '\nSize file: ' + str(os.stat(to_file).st_size / 1024) + '\n')
 
     elif x.split('.')[-1] == 'jpg':
         from_file = os.path.join('needs_sorting', x)
         to_file = os.path.join('images', x)
         os.rename(from_file, to_file)
         with open('images/create_folder_images.txt', 'a', encoding='utf8') as write_file:
-            write_file.write('\nName file: ' + x + '\nDate created file: ' + str(os.stat(to_file).st_ctime) + '\nSize file: ' + str(os.stat(to_file).st_size) + '\n')
+            write_file.write('\nName file: ' + x + '\nDate created file: %s' % time.ctime(os.path.getctime(to_file)) + '\nSize file: ' + str(os.stat(to_file).st_size / 1024) + '\n')
